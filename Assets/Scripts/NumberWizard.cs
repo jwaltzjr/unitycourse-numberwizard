33f34248b4871abb30c6dc832d1fac3039590205
jwaltzjr <jwaltzjr@hotmail.com>
2017-02-22 18:05:03 -0600

    Add min and max numbers

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -5,8 +5,14 @@ public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		print("Welcome to Nuber Wizard");
-		print("Pick a number in your head, but don't tell me!");
+		int min = 1;
+		int max = 1000;
+	
+		print ("Welcome to Nuber Wizard");
+		print ("Pick a number in your head, but don't tell me!");
+		
+		print ("The highest number you can pick is " + max);
+		print ("The lowest number you can pick is " + min);
 	}
 	
 	// Update is called once per frame
