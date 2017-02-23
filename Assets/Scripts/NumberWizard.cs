5bd97dcccd4723ce9d70f4414fe8fdb36fdd59f7
jwaltzjr <jwaltzjr@hotmail.com>
2017-02-22 18:20:50 -0600

    Add keyboard input

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -13,10 +13,21 @@ public class NumberWizard : MonoBehaviour {
 		
 		print ("The highest number you can pick is " + max);
 		print ("The lowest number you can pick is " + min);
+		
+		print ("Is the number higher or lower than 500?");
+		print ("[Up = higher, [Down] = lower, [Enter] = equal");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			print ("[Up] was pressed");
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			print ("[Down] was pressed");
+		}
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			print ("[Enter] was pressed");
+		}
 	}
 }
